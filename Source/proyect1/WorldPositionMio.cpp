0721e12b1dcd1fb92b90960f65ae277e2dcb71ba
mantonc01 <91096244+mantonc01@users.noreply.github.com>
2022-02-14 18:36:24 +0100

    antes de la interpolacion

--- Source/proyect1/WorldPositionMio.cpp
+++ Source/proyect1/WorldPositionMio.cpp
@@ -2,6 +2,7 @@
 
 
 #include "WorldPositionMio.h"
+#include "GameFramework/Actor.h"
 
 #include "IDetailTreeNode.h"
 
@@ -20,9 +21,34 @@ UWorldPositionMio::UWorldPositionMio()
 void UWorldPositionMio::BeginPlay()
 {
 	Super::BeginPlay();
+	UE_LOG(LogTemp,Warning,TEXT("%s"),*GetOwner()->GetTransform().GetLocation().ToString());
 
 	
-	UE_LOG(LogTemp,Warning,TEXT("Esto es un Warning"));
+	//UE_LOG(LogTemp,Warning,TEXT("Esto es un Warning"));
+	//UE_LOG(LogTemp,Error,TEXT("Esto es un mensaje de ERROR"));
+	//UE_LOG(LogTemp,Display,TEXT("Esto es un mensaje DE DISPLAY"));
+	
+	//FString Log=TEXT("HOLA");
+	/*FString* PtroLog= &Log;
+	FString NombreObjeto= GetOwner()->GetName();;
+	
+	int32 longitud1 =Log.Len();
+	int32 longitud2 =(*PtroLog).Len();
+	int32 longitud3 =PtroLog->Len();*/
+	
+	/*UE_LOG(LogTemp,Warning,TEXT("%d",longitud1));
+	UE_LOG(LogTemp,Warning,TEXT("%d",longitud2));
+	UE_LOG(LogTemp,Warning,TEXT("%d",longitud3));
+	
+	UE_LOG(LogTemp,Warning,TEXT("%s",*Log));
+	UE_LOG(LogTemp,Warning,TEXT("%s",**PtroLog));
+	*/
+
+	/*UE_LOG(LogTemp,Warning,TEXT("%s",*GetOwner()->GetName()));*/
+	//UE_LOG(LogTemp,Warning,TEXT("%s",NombreObjeto));
+
+	
+	
 	
 }
 
